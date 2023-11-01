49fbefcf01fee6d008d975e411459c92c413c9ff
Yan Daffa Putra Liandhie <144084303+YanDaffaPutraLiandhie@users.noreply.github.com>
2023-11-01 09:45:30 +0700

    Delete ArrayBilanganmod29.java

--- ArrayBilanganmod29.java
+++
@@ -1,9 +0,0 @@
-public class ArrayBilanganmod29{
-    public static void main(String[] args) {
-        int[] bil = {5,13,-7,17};
-
-        for (int i = 0; i<=4; i++){
-            System.out.println(bil[i]);
-        }
-    }
-}
